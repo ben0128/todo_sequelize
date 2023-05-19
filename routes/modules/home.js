c57091e90cf40282df24253b0d67bd11e3c93217
Banwind <a84012807@gmail.com>
2023-05-19 19:27:49 +0800

    feat: complete router todos.js

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -4,10 +4,9 @@ const db = require("../../models");
 const Todo = db.Todo
 
 router.get("/", (req, res) => {
-  const userId = req.user.id;
-  console.log(userId);
+  const UserId = req.user.id;
   return Todo.findAll({ 
-      where: userId,
+      where: { UserId },
       raw: true,
       nest: true
     })
--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -3,65 +3,63 @@ const router = express.Router();
 const db = require("../../models");
 const Todo = db.Todo
 
-router.get("/:id", (req, res) => {
-  const id = req.params.id;
-  return Todo.findByPk(id)
-    .then((todo) => res.render("detail", { todo: todo.toJSON() }))
-    .catch((error) => console.log(error));
+//進入新增紀錄的頁面
+router.get("/new", (req, res) => {
+  return res.render('new')
 });
 
+//新增一筆紀錄
 router.post("/", (req, res) => {
-  const userId = req.user._id;
   const name = req.body.name;
+  const userId = req.user.id;
 
-  return Todo.create({ name, userId })
+  return Todo.create({ name, UserId: userId, isDone : '0' })
     .then(() => res.redirect("/"))
     .catch((error) => console.log(error));
 });
 
 //瀏覽單筆todo的細節
 router.get('/:id', (req, res) => {
-  const userId = req.user._id
-  const _id = req.params.id
-  return Todo.findOne({ _id, userId })
-    .lean()
+  const userId = req.user.id
+  const id = req.params.id
+  return Todo.findOne({ where: { id, userId } })
     .then((todo) => res.render('detail', { todo }))
     .catch(error => console.log(error))
 })
 
 //進入編輯todo的畫面
 router.get('/:id/edit', (req, res) => {
-  const userId = req.user._id
-  const _id = req.params.id
-  return Todo.findOne({ _id, userId })
-    .lean()
-    .then((todo) => res.render('edit', { todo }))
-    .catch(error => console.log(error))
+  const userId = req.user.id
+  const id = req.params.id
+  return Todo.findOne({ where: { id, userId } })
+    .then((todo) => res.render("edit", { todo }))
+    .catch((error) => console.log(error));
 })
 
 //更新一筆todo
 router.put('/:id',(req, res) => {
-  const userId = req.user._id
-  const _id = req.params.id
+  const userId = req.user.id
+  const id = req.params.id
   const { name, isDone } = req.body
-  return Todo.findOne({ _id, userId })
+
+  Todo.findOne({ where: { id, userId }})
     .then(todo => {
       todo.name = name
       todo.isDone = isDone === 'on'
       return todo.save()
     })
-    .then(() => res.redirect(`/todos/${_id}`))
+    .then(() => res.redirect(`/todos/${id}`))
     .catch(error => console.log(error))
 })
 
 //刪除一筆資料
 router.delete('/:id', (req, res) => {
-  const userId = req.user._id
-  const _id = req.params.id
-  return Todo.findOne({ _id, userId })
-    .then(todo => todo.remove())
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
+  const userId = req.user.id
+  const id = req.params.id
+  return Todo.findOne({ where: { id, userId } })
+    .then((todo) => todo.remove())
+    .then(() => res.redirect("/"))
+    .catch((error) => console.log(error));
 })
 
 module.exports = router;
