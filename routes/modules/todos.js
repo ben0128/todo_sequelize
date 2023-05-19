5d7251d5d7daa89cb784864048b4bae8c2376b11
Banwind <a84012807@gmail.com>
2023-05-19 19:48:46 +0800

    feat: complete router delete

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -11,53 +11,54 @@ router.get("/new", (req, res) => {
 //新增一筆紀錄
 router.post("/", (req, res) => {
   const name = req.body.name;
-  const userId = req.user.id;
+  const UserId = req.user.id;
 
-  return Todo.create({ name, UserId: userId, isDone : '0' })
+  return Todo.create({ name, UserId, isDone : '0' })
     .then(() => res.redirect("/"))
     .catch((error) => console.log(error));
 });
 
 //瀏覽單筆todo的細節
 router.get('/:id', (req, res) => {
-  const userId = req.user.id
+  const UserId = req.user.id;
   const id = req.params.id
-  return Todo.findOne({ where: { id, userId } })
-    .then((todo) => res.render('detail', { todo }))
+  return Todo.findOne({ where: { id, UserId } })
+    .then((todo) => res.render('detail', { todo:todo.toJSON() }))
     .catch(error => console.log(error))
 })
 
 //進入編輯todo的畫面
 router.get('/:id/edit', (req, res) => {
-  const userId = req.user.id
+  const UserId = req.user.id;
   const id = req.params.id
-  return Todo.findOne({ where: { id, userId } })
+  return Todo.findOne({ where: { id, UserId } })
     .then((todo) => res.render("edit", { todo }))
     .catch((error) => console.log(error));
 })
 
 //更新一筆todo
 router.put('/:id',(req, res) => {
-  const userId = req.user.id
+  console.log('23232323')
+  const UserId = req.user.id;
   const id = req.params.id
   const { name, isDone } = req.body
 
-  Todo.findOne({ where: { id, userId }})
-    .then(todo => {
-      todo.name = name
-      todo.isDone = isDone === 'on'
-      return todo.save()
+  Todo.findOne({ where: { id, UserId } })
+    .then((todo) => {
+      todo.name = name;
+      todo.isDone = isDone === "on";
+      return todo.save();
     })
     .then(() => res.redirect(`/todos/${id}`))
-    .catch(error => console.log(error))
+    .catch((error) => console.log(error));
 })
 
 //刪除一筆資料
 router.delete('/:id', (req, res) => {
-  const userId = req.user.id
+  const UserId = req.user.id;
   const id = req.params.id
-  return Todo.findOne({ where: { id, userId } })
-    .then((todo) => todo.remove())
+  return Todo.findOne({ where: { id, UserId } })
+    .then((todo) => todo.destroy())
     .then(() => res.redirect("/"))
     .catch((error) => console.log(error));
 })
