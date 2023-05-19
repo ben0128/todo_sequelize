cdc8d22fd5e7c9d66d701e219cfe2695d227fbb8
Banwind <a84012807@gmail.com>
2023-05-19 16:53:21 +0800

    feat: add home.js

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -5,10 +5,13 @@ const Todo = require("../../models/todo");
 router.get("/", (req, res) => {
   const userId = req.user._id;
 
-  return Todo.findAll({ where: userId })
-    .sort(sortOption)
-    .then((todo) => res.render("index", { todo }))
-    .catch((error) => console.error(error));
+  return Todo.findAll({ 
+      where: userId,
+      raw: true,
+      nest: true
+    })
+    .then((todos) => {return res.render("index", { todos })})
+    .catch((error) => { return res.status(422).json(error) });
 });
 
 module.exports = router;
